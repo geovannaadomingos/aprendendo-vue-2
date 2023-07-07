3bbd6f9d6a19961dbfb2aa4af758216ae51e6dda
Geovanna Mayra Domingos do Nascimento <gmdomingosn@gmail.com>
2023-07-07 15:55:03 -0300

    adicionando métodos utilizando diretivas no componente Info.vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -10,9 +10,15 @@
             <li>HTML</li>
             <li>CSS</li>
         </ul>
-          <!-- diferentemente do if,v-bind:href="meu_link"  a diretiva v-show não tem uma alternativa, ou seja, não tem um else. Mostra ou não mostra, sem o caso se mostrar algo alternativo  -->
+        <div>
+            <!-- utilizando a diretiva click para executar um método -->
+            <button @click="show_email">{{texto_botao}}</button>
+        </div>
+        <!-- diferentemente do if,v-bind:href="meu_link"  a diretiva v-show não tem uma alternativa, ou seja, não tem um else. Mostra ou não mostra, sem o caso se mostrar algo alternativo  -->
         <p v-show="mostrar_email">E-mail: {{email}}</p>
+
         <!--  utilizando a diretiva v-bind para pegar dinamicamente o valor do href     -->
+
         <p>Para ver mais informções sobre mim, acesse o meu  <a v-bind:href="linkedin" target="_blank" >LinkedIN</a></p>
         
     </div>
@@ -26,7 +32,16 @@ export default {
             esta_trabalhando: true,
             mostrar_email: false,
             email: "gmdomingosn@gmail.com",
-            linkedin:"https://br.linkedin.com/in/geovanna-domingos-b31b79190"
+            linkedin:"https://br.linkedin.com/in/geovanna-domingos-b31b79190",
+            texto_botao: "Mostrar e-mail"
+        }
+    },
+    methods:{
+        show_email(){
+            this.mostrar_email = !this.mostrar_email
+            if(this.mostrar_email){
+                this.texto_botao = "Esconder e-mail"
+            }
         }
     }
 }
