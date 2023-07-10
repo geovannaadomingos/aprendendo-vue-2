7f38c4ae98ddc3318997b0b7a6ddd61f5f9e078c
Geovanna Mayra Domingos do Nascimento <gmdomingosn@gmail.com>
2023-07-10 12:20:33 -0300

    adiciona manipulação de arrays e objetos com a diretiva v-for no Info.vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,12 +3,14 @@
         <!-- utilizando a diretiva v-if para tornar a tela dinâmica e condicionar o aparecimento de informações. Essa diretiva pode estra sozinha, ou vir acompanhada da diretiva else, logo abaixo-->
         <p v-if="esta_trabalhando">No momento, estou trabalhando com as seguintes tecnologias:</p>
         <p v-else>Estudo as seguintes tecnologias:</p>
+        <!-- Utilizando a diretiva v-for para exibir os elementos de um array (tecnologias) que está no script da página-->
         <ul>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>Python</li>
-            <li>HTML</li>
-            <li>CSS</li>
+            <li v-for="(tecnologia, index) in tecnologias" v-bind:key="index">{{tecnologia}}</li>
+        </ul>
+        <p>Algumas das minhas habilidades pessoais são:</p>
+        <!-- Utilizando a diretiva v-for para exibir os elemento de array de objetos -->
+        <ul>
+            <li v-for="elemento in habilidades_pessoais" :key="elemento.id"> {{ elemento.habilidade }}</li>
         </ul>
         <div>
             <!-- utilizando a diretiva click para executar um método -->
@@ -33,7 +35,13 @@ export default {
             mostrar_email: false,
             email: "gmdomingosn@gmail.com",
             linkedin:"https://br.linkedin.com/in/geovanna-domingos-b31b79190",
-            texto_botao: "Mostrar e-mail"
+            texto_botao: "Mostrar e-mail",
+            tecnologias: ["JavaScript", "React.js", "TypeScript", "HTML", "CSS", "Git", "Python"],
+            habilidades_pessoais:[
+                {id: 1, habilidade:"Comunicação"},
+                {id: 2, habilidade:"Trabalho em equipe"},
+                {id: 2, habilidade:"Proativade"}
+            ]
         }
     },
     methods:{
